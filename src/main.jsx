af1383fa0147034b4d3c81395ea6338354b70322
RIPON KHAN <riponakondo4521@gmail.com>
2023-11-06 19:55:14 +0600

    create neasted routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App></App>
+    // errorElement: <ErrorPage />,
+    // loader: rootLoader,
+    // children: [
+    //   {
+    //     path: "contacts/:contactId",
+    //     element: <Contact />,
+    //   },
+    // ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+ <RouterProvider router={router}>
+ <App />
+ </RouterProvider>
   </React.StrictMode>,
 )
